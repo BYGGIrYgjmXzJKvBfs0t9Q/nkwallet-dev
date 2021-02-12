c7970eacc5c81328c3b30f28e93d543ceadbd23c
admin <admin>
2021-02-12 03:04:35 -0500

    init

---
+++ src/renderer/nuxt.config.js
@@ -0,0 +1,26 @@
+/**
+ * By default, Nuxt.js is configured to cover most use cases.
+ * This default configuration can be overwritten in this file
+ * @link {https://nuxtjs.org/guide/configuration/}
+ */
+
+
+module.exports = {
+  mode: 'spa', // or 'universal'
+  head: {
+    title: 'nkdev',
+    meta: [{ charset: "utf-8" }]
+  },
+  loading: false,
+  plugins: [
+    
+    
+    
+  ],
+  buildModules: [
+    
+  ],
+  modules: [
+    
+  ],
+};

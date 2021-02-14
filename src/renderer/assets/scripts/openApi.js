f601915910b679cb73975e67467721f389085e1a
admin <admin>
2021-02-14 04:48:14 -0500

    2

--- src/renderer/assets/scripts/openApi.js
+++ src/renderer/assets/scripts/openApi.js
@@ -1,12 +1,19 @@
+import axios from 'axios'
 
-
-const webPrefix = 'https://nscan.io/'
 const prefix = 'https://openapi.nkn.org/api/v1/'
+const webPrefix = 'https://nscan.io/'
 
-function transactions (input) {
-  return `${prefix}addresses/${input}/transactions`
+export async function fetchWallet (input) {
+  const wallet = await axios.get(`${prefix}addresses/${input}`)
+  const transactions = await axios.get(`${prefix}addresses/${input}/transactions`)
+  const webUrl = `${webPrefix}addresses/${input}`
+  console.log({ ...wallet.data, ...transactions.data, webUrl})
+  return { ...wallet.data, ...transactions.data, webUrl}
 }
 
-function addressInfo (input) {
-  return `${prefix}addresses/${input}`
+export async function fetchTransaction (input) {
+  const transaction = await axios.get(`${prefix}transactions/${input}`)
+  const webUrl = `${webPrefix}transactions/${input}`
+  console.log({ ...transaction.data, webUrl})
+  return { ...transaction.data, webUrl}
 }
